beccb9c37244ccd6e405cb0f97941f5d45b4b0b7
daniel.boiko@semdatex.com <Laborast52!>
2022-10-12 08:06:44 +0200

    added Crt row

--- src/main/java/SeleniumThresholds/CRTRow.java
+++ src/main/java/SeleniumThresholds/CRTRow.java
@@ -7,8 +7,7 @@ import static selenium.CompareTasksInCardio.driver;
 public class CRTRow {
     //is selected
     static boolean isCrtParameterSelected() {
-        boolean crtParameterIsChecked = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div[2]/table/tbody/tr[2]/td[1]/span/span[1]/input")).isSelected();
-        return crtParameterIsChecked;
+        return driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div[2]/table/tbody/tr[2]/td[1]/span/span[1]/input")).isSelected();
     }
 
 
