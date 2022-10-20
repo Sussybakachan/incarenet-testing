3515534ce9447b4eeddfd90535942f2ac5c6c371
Daniel Boiko <daniel.boiko@semdatex.com>
2022-10-20 09:57:24 +0200

    added first expected white task MicroPort, added compare iteration and added parameterisation of compare tasks method

---
+++ src/main/java/selenium/CreationOfExpectedLists.java
@@ -0,0 +1,11 @@
+package selenium;
+
+import static ExpectedTasks.MicroPortExpectedTasks.createMicroPortWhiteTask;
+import static selenium.ExpectedTasks.createAbbottTestExpectedTask;
+
+public class CreationOfExpectedLists {
+  public static void createExpectedTasks(){
+      createMicroPortWhiteTask();
+      createAbbottTestExpectedTask();
+  }
+}
--- src/main/java/selenium/StartTesting.java
+++ src/main/java/selenium/StartTesting.java
@@ -1,17 +1,15 @@
 package selenium;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Properties;
 
+import static ExpectedTasks.MicroPortExpectedTasks.createMicroPortWhiteTask;
 import static selenium.CompareTasksInCardio.*;
-import static selenium.ExpectedTasks.createTasks;
+import static selenium.CreationOfExpectedLists.createExpectedTasks;
+import static selenium.ExpectedTasks.createAbbottTestExpectedTask;
 import static selenium.ExpectedTasks.expectedTask;
 
 
@@ -30,8 +28,8 @@ public class StartTesting {
         action = new Actions(driver);
         PasteFile.pasteFile("C:\\Users\\dboiko\\IdeaProjects\\Selenium-Plugin2\\hl7\\Atriale Arrhythmielast über dem Grenzwert.hl7", "C:\\File-Forwarder-Armee\\ff2\\input\\Atriale Arrhythmielast über dem Grenzwert.hl7");
 
-        createTasks();
-        System.out.println(createTasks());
+      createExpectedTasks();
+        System.out.println(createAbbottTestExpectedTask());
         System.out.println("expectedTask checkbox: " + expectedTask.getCheckBox() + expectedTask.getAction() + expectedTask.getTaskDescription() + expectedTask.getColor() + expectedTask.getEmployee() + expectedTask.getStartDate() + expectedTask.getReceiveDate() + expectedTask.getTargetDate() + expectedTask.getMeasurements() + expectedTask.getPdf() + expectedTask.getFurtherInformation());
         loginP();
         inCardioDash();
