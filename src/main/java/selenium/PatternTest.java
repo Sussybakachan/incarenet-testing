679d9510a418d60fee6f7096da7160478877e4b3
Daniel Boiko <daniel.boiko@semdatex.com>
2022-10-27 09:52:33 +0200

    Pattern now case-sensitive

--- src/main/java/selenium/PatternTest.java
+++ src/main/java/selenium/PatternTest.java
@@ -5,9 +5,10 @@ import java.util.regex.Pattern;
 
 public class PatternTest {
     // System.out.println(PatternTest.useRegex("2022-10-26T12:34:14.265Z"));
+    //returns true
     public static boolean useRegex(final String input) {
         // Compile regular expression
-        final Pattern pattern = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\\.[0-9]+)?([zZ]|([\\+-])([01]\\d|2[0-3]):?([0-5]\\d)?)?", Pattern.CASE_INSENSITIVE);
+        final Pattern pattern = Pattern.compile("^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\\.[0-9]{1,3})?Z$");
         // Match regex against input
         final Matcher matcher = pattern.matcher(input);
         // Use results...
