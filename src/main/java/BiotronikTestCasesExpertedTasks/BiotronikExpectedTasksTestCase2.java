0d842c4ce926f76b9a98b04d1f446655cf9eb520
Daniel Boiko <daniel.boiko@semdatex.com>
2022-10-28 14:02:22 +0200

    added an error if the ExpectedTasksList did not get created

--- src/main/java/BiotronikTestCasesExpertedTasks/BiotronikExpectedTasksTestCase2.java
+++ src/main/java/BiotronikTestCasesExpertedTasks/BiotronikExpectedTasksTestCase2.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class BiotronikExpectedTasksTestCase2 {
     //TestCase2
+
+    //TODO Allgemeine Alarme,ERI und EOS
     static Task Biotronik93AlarmTask = new Task();
     static Task Biotronik93LvTask = new Task();
 
@@ -19,14 +21,10 @@ public class BiotronikExpectedTasksTestCase2 {
     static Task BiotronikEOSAlarmTask = new Task();
 
     static Task BiotronikERIAlarmTask = new Task();
-    public static List<Task> BiotronikTestCase1List = new ArrayList<>();
+    public static List<Task> BiotronikTestCase2List = new ArrayList<>();
 
 
-    public static void createExpectedTasksBioTestCase1() {
-        createBiotronik97CrtTask();
-        createBiotronik97LvTask();
-        createBiotronik97generalTask();
-    }
+
     //instead of calling all the create methods in one big create methode we could create
     //new objects of the classes
     //I don't know if that makes a difference
@@ -50,40 +48,108 @@ public class BiotronikExpectedTasksTestCase2 {
         Biotronik93AlarmTask.setFurtherInformation("");
         Biotronik93AlarmTask.setAction("Aufgabe an PBA weiterleiten");
 
-        BiotronikTestCase1List.add(Biotronik93AlarmTask);
+        BiotronikTestCase2List.add(Biotronik93AlarmTask);
     }
-    public static  List<Task> createBiotronik97LvTask() {
+    public static  List<Task> createBiotronik93LvTask() {
 
         Biotronik93LvTask.setCheckBox("");
-        Biotronik93LvTask.setColor("warning");
-        Biotronik93LvTask.setStartDate("2022-04-27T02:12:55.000Z");
-        Biotronik93LvTask.setReceiveDate("2022-10-26T12:34:14.265Z");
-        Biotronik93LvTask.setTargetDate("2022-10-27T12:34:14.265Z");
-        Biotronik93LvTask.setTaskDescription("LV-Stimulation: 97%");
+        Biotronik93LvTask.setColor("danger");
+        Biotronik93LvTask.setStartDate("2022-03-27T23:58:06.000Z");
+       // Biotronik93LvTask.setReceiveDate("2022-10-26T12:34:14.265Z");
+       // Biotronik93LvTask.setTargetDate("2022-10-27T12:34:14.265Z");
+        Biotronik93LvTask.setTaskDescription("LV-Stimulation: 93%");
         Biotronik93LvTask.setMeasurements("Bitte auswählen");
-        Biotronik93LvTask.setPdf("https://www.biotronik-homemonitoring.com/hmsc_guiWeb/qs/2c9881db78d1c30d0178f424e17e4a2fStatus_report_11525_11228833_2022-04-27_04-12-55.pdf");
+        Biotronik93LvTask.setPdf("https://www.biotronik-homemonitoring.com/hmsc_guiWeb/qs/2c9881dc78d1c39201790d5929842afeStatus_report_36682_11228833_2022-03-28_01-58-06.pdfEpisode_report_11228833_417_2022-03-28_01-58-06.pdfEpisode_report_11228833_414_2022-03-28_01-58-06.pdfEpisode_report_11228833_412_2022-03-28_01-58-06.pdfEpisode_report_11228833_408_2022-03-28_01-58-06.pdfEpisode_report_11228833_406_2022-03-28_01-58-06.pdfEpisode_report_11228833_396_2022-03-28_01-58-06.pdfEpisode_report_11228833_390_2022-03-28_01-58-06.pdfEpisode_report_11228833_389_2022-03-28_01-58-06.pdfEpisode_report_11228833_379_2022-03-28_01-58-06.pdfEpisode_report_11228833_367_2022-03-28_01-58-06.pdf");
         Biotronik93LvTask.setEmployee("");
         Biotronik93LvTask.setFurtherInformation("");
         Biotronik93LvTask.setAction("Aufgabe an PBA weiterleiten");
 
-        BiotronikTestCase1List.add(Biotronik93LvTask);
-        return BiotronikTestCase1List;
+        BiotronikTestCase2List.add(Biotronik93LvTask);
+        return BiotronikTestCase2List;
     }
-    public static  List<Task> createBiotronik97CrtTask() {
+    public static  List<Task> createBiotronik93CrtTask() {
 
         Biotronik93CrtTask.setCheckBox("");
-        Biotronik93CrtTask.setColor("warning");
-        Biotronik93CrtTask.setStartDate("2022-04-27T02:12:55.000Z");
-        Biotronik93CrtTask.setReceiveDate("2022-10-26T12:34:14.265Z");
-        Biotronik93CrtTask.setTargetDate("2022-10-27T12:34:14.265Z");
-        Biotronik93CrtTask.setTaskDescription("CRT-Stimulation: 97%");
+        Biotronik93CrtTask.setColor("danger");
+        Biotronik93CrtTask.setStartDate("2022-03-27T23:58:06.000Z");
+       // Biotronik93CrtTask.setReceiveDate("2022-10-26T12:34:14.265Z");
+        //Biotronik93CrtTask.setTargetDate("2022-10-27T12:34:14.265Z");
+        Biotronik93CrtTask.setTaskDescription("CRT-Stimulation: 93%");
         Biotronik93CrtTask.setMeasurements("Bitte auswählen");
-        Biotronik93CrtTask.setPdf("https://www.biotronik-homemonitoring.com/hmsc_guiWeb/qs/2c9881db78d1c30d0178f424e17e4a2fStatus_report_11525_11228833_2022-04-27_04-12-55.pdf");
+        Biotronik93CrtTask.setPdf("https://www.biotronik-homemonitoring.com/hmsc_guiWeb/qs/2c9881dc78d1c39201790d5929842afeStatus_report_36682_11228833_2022-03-28_01-58-06.pdfEpisode_report_11228833_417_2022-03-28_01-58-06.pdfEpisode_report_11228833_414_2022-03-28_01-58-06.pdfEpisode_report_11228833_412_2022-03-28_01-58-06.pdfEpisode_report_11228833_408_2022-03-28_01-58-06.pdfEpisode_report_11228833_406_2022-03-28_01-58-06.pdfEpisode_report_11228833_396_2022-03-28_01-58-06.pdfEpisode_report_11228833_390_2022-03-28_01-58-06.pdfEpisode_report_11228833_389_2022-03-28_01-58-06.pdfEpisode_report_11228833_379_2022-03-28_01-58-06.pdfEpisode_report_11228833_367_2022-03-28_01-58-06.pdf");
         Biotronik93CrtTask.setEmployee("");
         Biotronik93CrtTask.setFurtherInformation("");
         Biotronik93CrtTask.setAction("Aufgabe an PBA weiterleiten");
 
-        BiotronikTestCase1List.add(Biotronik93CrtTask);
-        return BiotronikTestCase1List;
+        BiotronikTestCase2List.add(Biotronik93CrtTask);
+        return BiotronikTestCase2List;
+    }
+    public static  List<Task> createBiotronikERITask() {
+
+        BiotronikERITask.setCheckBox("");
+        BiotronikERITask.setColor("warning");
+        BiotronikERITask.setStartDate("2022-04-27T02:12:55.000Z");
+        BiotronikERITask.setReceiveDate("2022-10-26T12:34:14.265Z");
+        BiotronikERITask.setTargetDate("2022-10-27T12:34:14.265Z");
+        BiotronikERITask.setTaskDescription("CRT-Stimulation: 97%");
+        BiotronikERITask.setMeasurements("Bitte auswählen");
+        BiotronikERITask.setPdf("https://www.biotronik-homemonitoring.com/hmsc_guiWeb/qs/2c9881db78d1c30d0178f424e17e4a2fStatus_report_11525_11228833_2022-04-27_04-12-55.pdf");
+        BiotronikERITask.setEmployee("");
+        BiotronikERITask.setFurtherInformation("");
+        BiotronikERITask.setAction("Aufgabe an PBA weiterleiten");
+
+        BiotronikTestCase2List.add(BiotronikERITask);
+        return BiotronikTestCase2List;
+    }
+    public static  List<Task> createBiotronikEOSTask() {
+
+        BiotronikEOSTask.setCheckBox("");
+        BiotronikEOSTask.setColor("warning");
+        BiotronikEOSTask.setStartDate("2022-04-27T02:12:55.000Z");
+        BiotronikEOSTask.setReceiveDate("2022-10-26T12:34:14.265Z");
+        BiotronikEOSTask.setTargetDate("2022-10-27T12:34:14.265Z");
+        BiotronikEOSTask.setTaskDescription("CRT-Stimulation: 97%");
+        BiotronikEOSTask.setMeasurements("Bitte auswählen");
+        BiotronikEOSTask.setPdf("https://www.biotronik-homemonitoring.com/hmsc_guiWeb/qs/2c9881db78d1c30d0178f424e17e4a2fStatus_report_11525_11228833_2022-04-27_04-12-55.pdf");
+        BiotronikEOSTask.setEmployee("");
+        BiotronikEOSTask.setFurtherInformation("");
+        BiotronikEOSTask.setAction("Aufgabe an PBA weiterleiten");
+
+        BiotronikTestCase2List.add(BiotronikEOSTask);
+        return BiotronikTestCase2List;
+    }
+    public static  List<Task> createBiotronikEOSAlarmTask() {
+
+        BiotronikEOSAlarmTask.setCheckBox("");
+        BiotronikEOSAlarmTask.setColor("warning");
+        BiotronikEOSAlarmTask.setStartDate("2022-04-27T02:12:55.000Z");
+        BiotronikEOSAlarmTask.setReceiveDate("2022-10-26T12:34:14.265Z");
+        BiotronikEOSAlarmTask.setTargetDate("2022-10-27T12:34:14.265Z");
+        BiotronikEOSAlarmTask.setTaskDescription("CRT-Stimulation: 97%");
+        BiotronikEOSAlarmTask.setMeasurements("Bitte auswählen");
+        BiotronikEOSAlarmTask.setPdf("https://www.biotronik-homemonitoring.com/hmsc_guiWeb/qs/2c9881db78d1c30d0178f424e17e4a2fStatus_report_11525_11228833_2022-04-27_04-12-55.pdf");
+        BiotronikEOSAlarmTask.setEmployee("");
+        BiotronikEOSAlarmTask.setFurtherInformation("");
+        BiotronikEOSAlarmTask.setAction("Aufgabe an PBA weiterleiten");
+
+        BiotronikTestCase2List.add(BiotronikEOSAlarmTask);
+        return BiotronikTestCase2List;
+    }
+    public static  List<Task> createBiotronikERIAlarmTask() {
+
+        BiotronikERIAlarmTask.setCheckBox("");
+        BiotronikERIAlarmTask.setColor("warning");
+        BiotronikERIAlarmTask.setStartDate("2022-04-27T02:12:55.000Z");
+        BiotronikERIAlarmTask.setReceiveDate("2022-10-26T12:34:14.265Z");
+        BiotronikERIAlarmTask.setTargetDate("2022-10-27T12:34:14.265Z");
+        BiotronikERIAlarmTask.setTaskDescription("CRT-Stimulation: 97%");
+        BiotronikERIAlarmTask.setMeasurements("Bitte auswählen");
+        BiotronikERIAlarmTask.setPdf("https://www.biotronik-homemonitoring.com/hmsc_guiWeb/qs/2c9881db78d1c30d0178f424e17e4a2fStatus_report_11525_11228833_2022-04-27_04-12-55.pdf");
+        BiotronikERIAlarmTask.setEmployee("");
+        BiotronikERIAlarmTask.setFurtherInformation("");
+        BiotronikERIAlarmTask.setAction("Aufgabe an PBA weiterleiten");
+
+        BiotronikTestCase2List.add(BiotronikERIAlarmTask);
+        return BiotronikTestCase2List;
     }
 }
