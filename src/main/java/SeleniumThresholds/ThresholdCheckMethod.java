93761124e3f4d110d9aad5b77d807577fb89bdf4
Daniel Boiko <daniel.boiko@semdatex.com>
2022-11-10 10:55:35 +0100

    added paramters for the thresholdvalues and created own class for ThresholdCheckMethod

--- src/main/java/SeleniumThresholds/ThresholdCheckMethod.java
+++ src/main/java/SeleniumThresholds/ThresholdCheckMethod.java
@@ -1,21 +1,32 @@
 package SeleniumThresholds;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import SeleniumThresholds.CRTRow.*;
 
+import java.util.List;
+
+import static SeleniumThresholds.CRTRow.findingParamActivated;
+import static SeleniumThresholds.CRTRow.pressCrtFindingCheckboxAndAddValue;
 import static selenium.CompareTasksInCardio.driver;
 
 public class ThresholdCheckMethod {
     public static void thresholdCheck() throws InterruptedException {
         Thread.sleep(5000);
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div[2]/button")).click();
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div/div[2]/div/div/div/table/tbody/tr[2]")).click();       //showImplantTemplate
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("//div[@id='root']/div/div[2]//div[6]")).click();
+        //TODO I changed the path to the Schwellenwerte because he didn't find it with the other xpath
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[1]/div[6]")).click();     //Schwellenwerte
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//table/tbody/tr[9]/td[4]/div/button")).click();       //showImplantTemplate
+        //create a template if patient doesnt have one
+        List<WebElement> bearbeitenButtonAnzahl = driver.findElements(By.xpath("/html/body/div/div/div[2]/div[2]/div[2]/form/div[1]/div/div/span"));
+        if(bearbeitenButtonAnzahl.size() > 0){
+            driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div[2]/form/div[2]/div[2]/button/span[1]")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div[3]/table/tbody/tr[9]/td[1]/span/span[1]/input")).click();
+        }
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div[3]/table/tbody/tr[9]/td[4]/div/button")).click();       //showImplantTemplate
         Thread.sleep(2000);
-        //deselectAll();
+        findingParamActivated(80);
         Thread.sleep(2000);
         driver.findElement(By.xpath("//div/div/div[2]/div/button")).click();        //saveAndBack
         Thread.sleep(2000);
@@ -23,6 +34,6 @@ public class ThresholdCheckMethod {
         Thread.sleep(2000);
 
         System.out.println("Threshold Value modified");
-
+        }
     }
-}
+
