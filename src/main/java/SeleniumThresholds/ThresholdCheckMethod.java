2a7987647ecf45fa35c4295b034507f3aa5c34e1
Daniel Boiko <daniel.boiko@semdatex.com>
2022-11-18 08:52:47 +0100

    rebase -> accepted branch 40 Thresholdcheck

--- src/main/java/SeleniumThresholds/ThresholdCheckMethod.java
+++ src/main/java/SeleniumThresholds/ThresholdCheckMethod.java
@@ -2,16 +2,16 @@ package SeleniumThresholds;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import SeleniumThresholds.CRTRow.*;
 
 import java.util.List;
 
-import static SeleniumThresholds.BatteryStatusRow.onlyEriEosActivated;
-import static SeleniumThresholds.CRTRow.findingParamActivated;
-import static SeleniumThresholds.CRTRow.onlyFindingCriticalActivated;
-import static SeleniumThresholds.LVRow.onlyFindingCriticalActivatedLv;
+import static SeleniumThresholds.BatteryStatusRow.*;
+import static SeleniumThresholds.CRTRow.*;
+import static SeleniumThresholds.LVRow.*;
 import static SeleniumThresholds.NoMeasurementsRow.onlyFindingCriticalActivatedNM;
-import static SeleniumThresholds.TelemABBRepRow.findingParamActivatedAbb;
-import static SeleniumThresholds.TelemABBRepRow.onlyParamActivatedAbb;
+import static SeleniumThresholds.TelemABBRepRow.*;
+import static SeleniumThresholds.TelemABBRepRow.*;
 import static selenium.CompareTasksInCardio.driver;
 
 public class ThresholdCheckMethod {
@@ -22,7 +22,7 @@ public class ThresholdCheckMethod {
         Thread.sleep(2000);
         //create a template if patient doesnt have one
         List<WebElement> bearbeitenButtonAnzahl = driver.findElements(By.xpath("/html/body/div/div/div[2]/div[2]/div[2]/form/div[1]/div/div/span"));
-        if (bearbeitenButtonAnzahl.size() > 0) {
+        if(bearbeitenButtonAnzahl.size() > 0){
             driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div[2]/form/div[2]/div[2]/button/span[1]")).click();
             Thread.sleep(2000);
             driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div[3]/table/tbody/tr[9]/td[1]/span/span[1]/input")).click();
@@ -30,24 +30,45 @@ public class ThresholdCheckMethod {
         Thread.sleep(5000);
         driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div[3]/table/tbody/tr[9]/td[4]/div/button")).click();       //showImplantTemplate
         Thread.sleep(2000);
-        if (manufacturer.equals("Abbott")) {
-            switch (testCase) {
-                case 1:
-                    findingParamActivatedAbb();
+        if (manufacturer.equals("Abbott")){
+            switch (testCase){
+                case 1: findingParamActivatedAbb();
+                break;
+                case 2: onlyParamActivatedAbb();
+                break;
+                case 3: findingParamActivated(96, 85);
+                        Thread.sleep(2000);
+                        findingParamActivatedAbb();
+                break;
+                case 4: onlyEriEosActivated();
+                        onlyFindingCriticalActivated(97, 85);
+                        onlyFindingCriticalActivatedLv(97, 85);
+                        onlyFindingCriticalActivatedNM(1,2);
+                case 5: onlyBatteryStatusActivated();
+                    onlyParamActivated(97,85); //CRT
+                    onlyParamActivatedLv(97,85);
+                    onlyFindingCriticalActivatedNM(1,2);
                     break;
-                case 2:
-                    onlyParamActivatedAbb();
+                case 6: BatteryStatusRow.activateAll();
+                    CRTRow.activateAll(97,85);
+                    activateAllLv(97,85);
+                    onlyFindingCriticalActivatedNM(1,2);
                     break;
-                case 3:
-                    findingParamActivated(96, 85);
-                    Thread.sleep(2000);
-                    findingParamActivatedAbb();
+                case 7: BatteryStatusRow.activateAll();
+                    onlyCriticalParamActivated(97,85); //CRT
+                    criticalParamActivatedLv(97,85);
+                    onlyFindingCriticalActivatedNM(1,2);
                     break;
-                case 4:
-                    onlyEriEosActivated();
-                    onlyFindingCriticalActivated(97, 85);
-                    onlyFindingCriticalActivatedLv(97, 85);
-                    onlyFindingCriticalActivatedNM(1, 2);
+                case 8: BatteryStatusRow.activateAll();
+                    findingParamActivated(97,85);
+                    findingParamActivatedLv(97, 85);
+                    onlyFindingCriticalActivatedNM(1,2);
+                    break;
+                case 9:
+                    //TODO CRT and LV are completely off, but still need to have 97 and 85 as values
+                    criticalParamActivatedAbb();
+                    onlyFindingCriticalActivatedNM(1,2);
+                    break;
             }
         }
 
@@ -58,6 +79,6 @@ public class ThresholdCheckMethod {
         Thread.sleep(2000);
 
         System.out.println("Threshold Value modified");
+        }
     }
-}
 
