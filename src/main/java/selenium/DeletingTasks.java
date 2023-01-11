7b2a3a0b34866ce819d96cb3d7b28044d2cbce7f
Fernand <fernand.mushinzimana@semdatex.com>
2023-01-11 12:37:18 +0100

    All Abbott testcases are running

--- src/main/java/selenium/DeletingTasks.java
+++ src/main/java/selenium/DeletingTasks.java
@@ -19,34 +19,35 @@ public class DeletingTasks {
 
     public static void deleteTask() throws InterruptedException {
         try{
-        js = (JavascriptExecutor) driver;
-        action = new Actions(driver);
-        Thread.sleep(5000);
-        List<WebElement> l = driver.findElements(By.xpath("//table/tbody[@class]/tr[@index]"));
-        int s = l.size();
-        js.executeScript("window.scrollBy(0,50)");
-        Thread.sleep(4000);
-        for (int i = 2; i < s + 2; i++) {
+            js = (JavascriptExecutor) driver;
+            action = new Actions(driver);
+            Thread.sleep(5000);
+            List<WebElement> l = driver.findElements(By.xpath("//table/tbody[@class]/tr[@index]"));
+            int s = l.size();
             Thread.sleep(4000);
-            driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div/div/table/tbody/tr[" + i + "]/td[7]/div/div/div/div")).click();
-            Thread.sleep(3000);
-            driver.findElement(By.xpath("/html/body/div[4]/div/div[1]/label/span[1]/span[1]/input")).click();
-            Thread.sleep(3000);
-            driver.findElement(By.id("saveMeasurement")).click();
-            Thread.sleep(7000);
-            driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div/div/table/tbody/tr[" + i + "]/td[11]/div/button[1]")).click();
+            js.executeScript("window.scrollBy(0,50)");
             Thread.sleep(4000);
-        }
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div/div/table/thead/tr/th[1]/span/span[1]/input")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.id("archiveTasksButton")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.id("confirmButton")).click();
-        Thread.sleep(5000);
-        js.executeScript("window.scrollBy(0,-50)");
-        Thread.sleep(4000);
-    } catch (Exception e) {
+            for (int i = 2; i < s + 2; i++) {
+                Thread.sleep(4000);
+                driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div/div/table/tbody/tr[" + i + "]/td[7]/div/div/div/div")).click();
+                Thread.sleep(3000);
+                driver.findElement(By.xpath("/html/body/div[4]/div/div[1]/label/span[1]/span[1]/input")).click();
+                Thread.sleep(3000);
+                driver.findElement(By.id("saveMeasurement")).click();
+                Thread.sleep(7000);
+                driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div/div/table/tbody/tr[" + i + "]/td[11]/div/button[1]")).click();
+                Thread.sleep(4000);
+            }
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div/div/table/thead/tr/th[1]/span/span[1]/input")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.id("archiveTasksButton")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.id("confirmButton")).click();
+            Thread.sleep(5000);
+            js.executeScript("window.scrollBy(0,-50)");
+            Thread.sleep(4000);
+        } catch (Exception e) {
             LoggingDataModif loggingDataModif = new LoggingDataModif();
             LoggerLoader.fatal("Some Error occurred while trying to delete the Tasks: " + e);
             LoggerLoader.info("Following Testcase(s) were successful: \n " + successfulTestCases);
