d221d11f1cf9bd56480325ca1239a8b4b259fe18
Daniel Boiko <daniel.boiko@semdatex.com>
2023-03-14 09:31:37 +0100

    Selenium now ignores .ignore files in the input folder (out-folder) and looks only after .hl7 and .xml files

--- src/main/java/selenium/PasteFile.java
+++ src/main/java/selenium/PasteFile.java
@@ -3,6 +3,8 @@ package selenium;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class PasteFile {
@@ -21,6 +23,48 @@ public class PasteFile {
 
     public static void inputIsEmpty() throws InterruptedException {
         File input = new File(System.getProperty("user.dir") + "/input");
+
+        List<String> extensions = Arrays.asList("hl7", "xml");
+
+        while (hasFilesWithExtensions(input, extensions)) {
+            try {
+                // Wait for 1 second before checking again
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("All files with extensions " + extensions + " have been processed or removed from the folder.");
+    }
+
+
+
+    private static boolean hasFilesWithExtensions(File input, List<String> extensions) {
+        // Get the list of files in the folder
+        File[] files = input.listFiles();
+
+        // Check if any of the files have the desired extensions
+        for (File file : files) {
+            String extension = getFileExtension(file);
+            if (extensions.contains(extension)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static String getFileExtension(File file) {
+        String name = file.getName();
+        int lastDotIndex = name.lastIndexOf('.');
+        if (lastDotIndex == -1) {
+            return "";
+        }
+        return name.substring(lastDotIndex + 1);
+    }
+
+/*
         while (!FileUtils.listFiles(input, null, true).isEmpty()) {
             // Wait for some time
             Thread.sleep(1000); // Wait for 1 second
@@ -29,5 +73,6 @@ public class PasteFile {
         Thread.sleep(3000);
 
     }
+    */
 
 }
