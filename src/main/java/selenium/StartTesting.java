ec1c3cfa3a14acdb79a9785ca7f7dcb4dfd80486
Daniel Boiko <daniel.boiko@semdatex.com>
2023-03-23 09:46:08 +0100

    had to remove some changes that I used for testing

--- src/main/java/selenium/StartTesting.java
+++ src/main/java/selenium/StartTesting.java
@@ -1,9 +1,13 @@
 package selenium;
 
+import TestCases.*;
 import log4j2.LoggerLoader;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Actions;
 
 import static selenium.ChromeWebDriver.driver;
 import static selenium.CompareTasksInCardio.*;
+import static selenium.CreationOfAllExpectedTasks.createAllExpectedTasks;
 
 
 public class StartTesting {
@@ -12,29 +16,50 @@ public class StartTesting {
     public static void main(String[] args) throws Exception {
         //  System.out.println(PatternTest.useRegex("2022-12-26T12:34:14.265Z"));
         //  System.out.println(PatternTest.useRegex("2022-12-26T12:34:14.265Z"));
-
+        LoggerLoader.info("Automation test started");
         InsuiteServerConfig insuiteServerConfig = new InsuiteServerConfig();
         ReadingConfig rc = null;
         rc = new ReadingConfig();
         ChromeWebDriver chromeWebDriver= new ChromeWebDriver();
         chromeWebDriver.initiateChromeWebDriver(rc);
         LoggerLoader.info("Weitergegangen");
-        System.out.println("irgendwas");
         try{
             insuiteServerConfig.getInSuiteURL();
         } catch (Exception e){
             LoggerLoader.fatal(String.valueOf(e));
         }
 
-Thread.sleep(1000000000);
-        LoggerLoader.info("Automation test started");
+        driver.manage().window().maximize();
+        LoggerLoader.info("Window maximized");
+        js = (JavascriptExecutor) driver;
+        action = new Actions(driver);
+        // PasteFile.pasteFile("C:\\Users\\dboiko\\IdeaProjects\\Selenium-Plugin2\\hl7\\Atriale Arrhythmielast über dem Grenzwert.hl7", "C:\\File-Forwarder-Armee\\ff2\\input\\Atriale Arrhythmielast über dem Grenzwert.hl7");
+
+        //createTasks();
+        createAllExpectedTasks();
+        // System.out.println("expectedTask checkbox: " + expectedTask.getCheckBox() + expectedTask.getAction() + expectedTask.getTaskDescription() + expectedTask.getColor() + expectedTask.getEmployee() + expectedTask.getStartDate() + expectedTask.getReceiveDate() + expectedTask.getTargetDate() + expectedTask.getMeasurements() + expectedTask.getPdf() + expectedTask.getFurtherInformation());
+        loginP();
+        inCardioDash();
+
+        //AllTestCasesPutTogether allTestCasesPutTogether = new AllTestCasesPutTogether();
+
+        AbbottTestCases abbottTestCases = new AbbottTestCases();
+        /*
+        //TODO method-> in utils.java
+        //BiotronikTestCases biotronikTestCases = new BiotronikTestCases();
+        //TODO method-> in utils.java
+        //BostonTestCases bostonTestCases = new BostonTestCases();
+        //TODO method-> in utils.java
+        //MedtronicTestCases medtronicTestCases = new MedtronicTestCases();
+        //TODO method-> in utils.java
+        //MicroPortTestCases microPortTestCases = new MicroPortTestCases();
+        // thresholdCheck();
+        //comparison(); */
         LoggingDataModif loggingDataModif = new LoggingDataModif();
         LoggerLoader.info("Automation test finished");
         LoggerLoader.info("Following Testcase(s) were successful: \n " + successfulTestCases);
         LoggerLoader.info("Following Testcase(s) failed:\n" + loggingDataModif.editFailedTaskLogging());
-
         driver.close();
-
 
     }
 
